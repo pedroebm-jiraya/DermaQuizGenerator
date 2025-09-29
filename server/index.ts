92d6e0421ba78c3bba7a17e872366dd69fce96ea
pedroebm-jiraya <pedroebm@gmail.com>
2025-09-29 20:32:37 -0300

    Fix: Configura host dinamicamente para deploy

--- server/index.ts
+++ server/index.ts
@@ -131,11 +131,13 @@ async function seedDatabaseIfEmpty() {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "localhost",
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+const port = process.env.PORT || 10000;
+
+server.listen({
+  port: parseInt(port), // Garante que a porta seja um número
+  host: host,
+}, () => {
+  console.log(`[express] serving on http://${host}:${port}`);
+});
 })();
